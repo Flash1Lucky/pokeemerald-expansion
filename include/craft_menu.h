408827f841d17b865a6081dc5c34e008d6fb34a0
Flash1Lucky <ericflashrussell@gmail.com>
2026-01-15 22:06:14 -0500

    Refactor recipe book logic to separate file
    Moved all recipe book menu logic from craft_menu.c to new craft_recipe_book.c and added corresponding header. Updated includes and function calls to use the new structure. Added documentation comments to clarify crafting system file responsibilities.

--- include/craft_menu.h
+++ include/craft_menu.h
@@ -1,7 +1,11 @@
 #ifndef GUARD_CRAFT_MENU_H
 #define GUARD_CRAFT_MENU_H
 
+#include "global.h"
+
 extern bool8 (*gMenuCallback)(void);
+extern const u8 gText_CraftSuccess[];
+void CraftMenu_SetCraftSuccessVars(u16 resultItemId, u16 resultQty);
 void CB2_OpenCraftMenu(void);
 void CB2_ReturnToCraftMenu(void);
 
